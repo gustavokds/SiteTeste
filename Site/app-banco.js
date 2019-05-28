b4890eca355ab2f9c42be2174c304c4947c74875
Aluno Bandtec <github@alunos.bandtec.com.br>
2019-05-28 18:04:38 -0300

    Ja cria um servidor local

---
+++ Site/app-banco.js
@@ -0,0 +1,45 @@
+var desenvolvimento = false;
+
+var configuracoes = {
+    producao: {
+        server: "bd-gus.database.windows.net",
+        user: "adm",
+        password: "#Gf47252577880",
+        database: "Bd-gus",
+        options: {
+            encrypt: true
+        },
+        pool: {
+            max: 4,
+            min: 1,
+            idleTimeoutMillis: 30000,
+            connectionTimeout: 5000
+        }
+    },
+    desenvolvimento: {
+        server: "BASETESTE.database.windows.net",
+        user: "usuariotestes",
+        password: "senhatestes",
+        database: "BASETESTE",
+        options: {
+            encrypt: true
+        }
+    }
+}
+ 
+var sql = require('mssql');
+sql.on('error', err => {
+    console.error(`Erro de Conexão: ${err}`);
+});
+
+var perfil = desenvolvimento ? 'desenvolvimento' : 'producao';
+
+function conectar() {
+  return sql.connect(configuracoes[perfil])
+  // return new sql.ConnectionPool();  
+} 
+
+module.exports = {
+    conectar: conectar,
+    sql: sql
+}
